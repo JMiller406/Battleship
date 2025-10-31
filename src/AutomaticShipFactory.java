0b0d9f0f8c26f1fdc7835fe6f850933fca949217
Luram Angel <luram.angel@msubillings.edu>
2025-10-30 19:40:41 -0600

    Updated AutomaticShip, Constants, Order classes

--- src/AutomaticShipFactory.java
+++ src/AutomaticShipFactory.java
@@ -1,3 +1,16 @@
+import java.util.Random;
+
 public class AutomaticShipFactory extends ShipFactory {
-    
+    // private final Random rand = new Random();
+
+    @Override
+    protected Coordinate getStartCoordinate(Ship ship) {
+
+        return Coordinate.randomCoordinate();
+    }
+
+    @Override
+    protected Direction getDirection() {
+        return Direction.randomDirection();
+    }
 }
